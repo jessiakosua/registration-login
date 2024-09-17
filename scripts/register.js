85f442bca5b23d380224b88c9e2528dee7873701
jessiakosua <jessica.a.adika@gmail.com>
2024-09-17 15:52:24 +0000

    added registration

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,23 @@
 // Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector("#register");
+console.log(registerForm);
 // handle form submit event
-registerForm.onsubmit = function( event) {
-event.preventDefault()
-console.log(event);
+registerForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // collect form data
+  const formData = new FormData(registerForm);
+  //send data to backend
+  //Display success message to user
+  const messageH1 = document.querySelector("#message");
+  messageH1.textContent = "Account registered successfully!";
+});
+
+//declare variable age
+const age = 71;
+//define squareAge function
+function squareAge(age) {
+  return age ** 2;
 }
+
+const squaredAge = squareAge(34);
+console.log(squaredAge);
